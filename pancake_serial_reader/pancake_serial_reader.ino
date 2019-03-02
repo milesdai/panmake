a367ef37265b5d183e185951f432fda58c0d46bd
Miles Dai <milesdai@mit.edu>
2019-03-02 18:43:59 -0500

    Added serial communication and stepper code - UNTESTED

---
+++ pancake_serial_reader/pancake_serial_reader.ino
@@ -0,0 +1,102 @@
+#include <string.h>
+#include <stdlib.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
+AccelStepper stepper1(AccelStepper::FULL4WIRE, 2, 3, 4, 5);
+AccelStepper stepper2(AccelStepper::FULL4WIRE, 8, 9, 10, 11);
+MultiStepper steppers;
+
+const float DIST_PER_STEP = 0.01; // CALIBRATE THIS! - physical distance per step
+
+int incomingByte = 0;
+const int CHAR_PER_INSTR = 50; // expect max 35 bytes + terminator
+const char END_MARKER = '!';
+
+char rxMessage[CHAR_PER_INSTR];
+int currentInstruction[8];
+
+bool newInstruction = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); 
+  
+  stepper1.setMaxSpeed(100);
+  stepper2.setMaxSpeed(100);
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
+}
+
+void loop() {
+  readInstruction();
+  parseInput();
+  if(newInstruction) {
+    executeInstruction();
+    newInstruction = false;
+  }
+  delay(5);
+}
+
+void readInstruction() {
+  char c;
+  char toProcess[CHAR_PER_INSTR];
+  int i = 0;
+  bool done = false;
+  if(Serial.available() > 0) {
+    while(Serial.available() > 0 && !done) {
+      c = Serial.read();
+      if (c != END_MARKER) {
+        rxMessage[i] = c;
+        i++;
+        if (i >=  CHAR_PER_INSTR) {
+          i--;
+          Serial.println("Error: Overflow");
+        } // overflow condition
+      } else {
+        rxMessage[i] = '\0';
+        i = 0;
+        done = true;
+        newInstruction = true;
+        return;
+      }
+    }
+  }
+  return 0;
+}
+
+void parseInput() {
+//  for(int i = 0; i < 4; i++)
+//    currentInstruction[i] = i;
+//  return;
+  const char delim[2] = ",";
+  char *token;
+
+  token = strtok(rxMessage, delim);
+  int i = 0;
+  
+  while(token != NULL) {
+    if (i > 3) {
+      Serial.println("Bad input, too many delimiters");
+      return 0;
+    }
+    currentInstruction[i] = atoi(token);
+    i++;
+    token = strtok(NULL, delim);
+  }
+  return 0;
+}
+
+void executeInstruction() {
+  for(int i = 0; i < 4; i++)
+    Serial.print(currentInstruction[i]);
+  Serial.print("\n");
+
+  long positions[] = {currentInstruction[0] / DIST_PER_STEP, 
+                      currentInstruction[1] / DIST_PER_STEP}; // Array of desired stepper positions
+
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition(); // Blocks until all are in position
+  delay(10); // can probably be removed
+}
+
--- serialWriter.py
+++ serialWriter.py
@@ -2,14 +2,24 @@ import serial
 import time 
 
 PORT_NAME = '/dev/cu.usbmodem14401'
-ser = serial.Serial(PORT_NAME, 9600, timeout=0)
+PORT_NAME = 'COM10'
+ser = serial.Serial(PORT_NAME, 115200, timeout=0)
 
 def sendMessage(message):
     '''message should be a byte string. Start and end characters appended automatically'''
-    message = b'$' + message + b'!'
+    # message = b'$' + message.encode('utf-8') + b'!'
+    message = message.encode('utf-8') + b'!'
     print('Writing to Arduino: ' + str(message))
     ser.write(message)
     ser.flush()
 
-sendMessage('50,20,100,1')
-sendMessage('60,70,150,0')
+def readMessage():
+    print("Reading from Arduino: " + str(ser.readline()))
+
+while True:
+    sendMessage('50,20,100,1')
+    readMessage()
+    time.sleep(2)
+    sendMessage('60,70,150,0')
+    readMessage()
+    time.sleep(2)
