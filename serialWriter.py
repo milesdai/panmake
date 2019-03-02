caf66583d0cba730d3a97033aec4d34e6c6bb31e
pundaval <Dodohead1#>
2019-03-02 15:29:21 -0500

    Modified serial writing code.

--- serialWriter.py
+++ serialWriter.py
@@ -1,9 +1,19 @@
 import serial
 import time 
 
-PORT_NAME = 'COM3'
+PORT_NAME = '/dev/cu.usbmodem14401'
 ser = serial.Serial(PORT_NAME, 9600, timeout=0)
 
-message = b'1'
+message = b'$1111111111111!'
 print('Writing: ' + str(message))
+
 ser.write(message)
+while True:
+    ser.flush
+    print(ser.readline())
+    time.sleep(0.5)
+    message = b'$1010101111100000!'
+    print('Writing: ' + str(message))
+
+    ser.write(message)
+    #ser.write(1)
